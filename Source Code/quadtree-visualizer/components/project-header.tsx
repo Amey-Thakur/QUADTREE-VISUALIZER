1b29a8ef1a110ed2b4d6fb789235957119cc990f
Amey Thakur <ameythakur20@gmail.com>
2026-01-06 15:16:23 -0500

    QuadTree Visualizer

--- Source Code/quadtree-visualizer/components/project-header.tsx
+++ Source Code/quadtree-visualizer/components/project-header.tsx
@@ -13,8 +13,19 @@ function ProjectHeader(props: ProjectHeaderProps): React.ReactElement {
         <div id={styles.title}>{props.title}</div>
         <div>Presented as part of the BE Major-Project @ Terna Engineering College</div>
         <div>
-          Project Authors: Amey Thakur, Hasan Rizvi, Mega Satish and Ajay Davare
-          (Group: Phi-CS-73, Batch of: {props.year})
+          Project Authors:{' '}
+          <a href='https://github.com/Amey-Thakur' target='_blank' rel='noopener noreferrer' className={styles.author_link}>
+            Amey Thakur
+          </a>
+          ,{' '}
+          <a href='https://github.com/rizvihasan' target='_blank' rel='noopener noreferrer' className={styles.author_link}>
+            Hasan Rizvi
+          </a>
+          ,{' '}
+          <a href='https://github.com/msatmod' target='_blank' rel='noopener noreferrer' className={styles.author_link}>
+            Mega Satish
+          </a>
+          {' '}and Ajay Davare (Group: Phi-CS-73, Batch of: {props.year})
         </div>
       </div>
     </div>

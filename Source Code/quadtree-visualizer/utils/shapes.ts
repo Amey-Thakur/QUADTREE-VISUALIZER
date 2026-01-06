72e4810a54bc71103f59b1206c619a6f1cc4bbe7
Amey Thakur <ameythakur20@gmail.com>
2026-01-06 16:17:57 -0500

    Standardize utils with scholarly authorship and documentation

--- Source Code/quadtree-visualizer/utils/shapes.ts
+++ Source Code/quadtree-visualizer/utils/shapes.ts
@@ -1,3 +1,13 @@
+/**
+ * Author: Amey Mega Hasan
+ * GitHub: https://github.com/Amey-Thakur
+ * Repo: https://github.com/Amey-Thakur/QUADTREE-VISUALIZER
+ * Year: 2021-2022
+ * License: MIT
+ *
+ * Defines the Rect class used for axis-aligned bounding box (AABB) geometric calculations.
+ */
+
 export class Rect {
   constructor(
     public x: number = 0,

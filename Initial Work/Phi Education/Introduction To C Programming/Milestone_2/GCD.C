fca9ea8a19b034bc97dcf61bf8d8f77c890e8885
Amey Thakur <ameythakur20@gmail.com>
2026-01-06 20:04:27 -0500

    QuadTree Visualizer

--- Initial Work/Phi Education/Introduction To C Programming/Milestone_2/GCD.C
+++ Initial Work/Phi Education/Introduction To C Programming/Milestone_2/GCD.C
@@ -1,29 +1,91 @@
-//Create a program for int gcd(int a, int b)-Calculates and returns the Greatest Common Divisor of a and b
+/**
+ * @file    GCD.C
+ * @brief   Calculates the Greatest Common Divisor (GCD) of two numbers.
+ *
+ * @author  Amey Thakur
+ *          GitHub: https://github.com/Amey-Thakur
+ * @author  Mega Satish
+ *          GitHub: https://github.com/msatmod
+ * @author  Hasan Rizvi
+ *          GitHub: https://github.com/rizvihasan
+ *
+ * @project QUADTREE-VISUALIZER
+ * @group   Phi-CS-73
+ * @batch   2022
+ * @repo    https://github.com/Amey-Thakur/QUADTREE-VISUALIZER
+ * @date    2021
+ * @license MIT
+ *
+ * Developed as part of the Phi Education Training (Milestone 2) and
+ * BE Major-Project @ Terna Engineering College, University of Mumbai.
+ *
+ * This program calculates the Greatest Common Divisor (GCD) of two integers.
+ * GCD is the largest positive integer that divides both numbers without
+ * remainder.
+ *
+ * @return  int  Returns 0 on successful execution.
+ */
 
-#include<stdio.h>
-#include<conio.h>
-int gcd(int,int);
-void main()
-{
- int a, b, ans;
- clrscr();
- printf("Enter the value for a:");
- scanf("%d", &a);
- printf("Enter the value for b:");
- scanf("%d", &b);
- ans=gcd(a,b);
- printf("GCD is : %d", ans);
- getch();
+#include <conio.h> /* Console I/O library for clrscr() and getch() (DOS/Turbo C specific) */
+#include <stdio.h> /* Standard I/O library for printf and scanf functions */
+
+/**
+ * @brief   Calculates the GCD of two numbers.
+ * @param   a  First integer.
+ * @param   b  Second integer.
+ * @return  int  The GCD of a and b.
+ */
+int gcd(int a, int b);
+
+/**
+ * @brief   Main function - Entry point of the program.
+ *
+ * Prompts the user to enter two numbers and displays their GCD.
+ */
+int main() {
+  int a, b, ans; /* a, b: input numbers; ans: GCD result */
+
+  clrscr(); /* Clears the console screen (Turbo C specific) */
+
+  /* Prompt user to enter first number */
+  printf("Enter the value for a:");
+  scanf("%d", &a);
+
+  /* Prompt user to enter second number */
+  printf("Enter the value for b:");
+  scanf("%d", &b);
+
+  /* Calculate GCD */
+  ans = gcd(a, b);
+
+  /* Display the GCD */
+  printf("GCD is : %d", ans);
+
+  getch(); /* Wait for a key press before closing (Turbo C specific) */
+
+  return 0; /* Return 0 to indicate successful execution */
 }
-int gcd(int a, int b)
-{
- int i, result;
- for(i=1;i<=a && i<=b;i++)
- {
-  if(a%i==0 && b%i==0)
-  {
-   result=i;
+
+/**
+ * @brief   Calculates the GCD of two numbers using iterative method.
+ *
+ * Iterates through all numbers from 1 to min(a, b) and finds the
+ * largest number that divides both a and b evenly.
+ *
+ * @param   a  First integer.
+ * @param   b  Second integer.
+ * @return  int  The GCD of a and b.
+ */
+int gcd(int a, int b) {
+  int i, result;
+
+  /* Check all numbers from 1 to minimum of a and b */
+  for (i = 1; i <= a && i <= b; i++) {
+    /* If i divides both a and b, it's a common divisor */
+    if (a % i == 0 && b % i == 0) {
+      result = i; /* Update result with the common divisor */
+    }
   }
- }
- return result;
+
+  return result;
 }

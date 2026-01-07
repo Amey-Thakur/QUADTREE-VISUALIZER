3b1e637d89253a1ab573306d84f9a27d19eb676a
Amey Thakur <ameythakur20@gmail.com>
2026-01-06 20:12:14 -0500

    QuadTree Visualizer

--- Initial Work/Phi Education/Introduction To C Programming/Milestone_5/strcompare.C
+++ Initial Work/Phi Education/Introduction To C Programming/Milestone_5/strcompare.C
@@ -1,50 +1,105 @@
-//write a program for int str_compare(char *str1, char *str2) - if str1 is less than str2 the function should return -1 if str1 is greater than str2 it should return 1 and if both are equal then return 0
+/**
+ * @file    strcompare.C
+ * @brief   Compares two strings and returns their relative order.
+ *
+ * @author  Amey Thakur
+ *          GitHub: https://github.com/Amey-Thakur
+ * @author  Mega Satish
+ *          GitHub: https://github.com/msatmod
+ * @author  Hasan Rizvi
+ *          GitHub: https://github.com/rizvihasan
+ *
+ * @project QUADTREE-VISUALIZER
+ * @group   Phi-CS-73
+ * @batch   2022
+ * @repo    https://github.com/Amey-Thakur/QUADTREE-VISUALIZER
+ * @date    2021
+ * @license MIT
+ *
+ * Developed as part of the Phi Education Training (Milestone 5) and
+ * BE Major-Project @ Terna Engineering College, University of Mumbai.
+ *
+ * This program compares two strings by calculating the sum of ASCII values.
+ * Returns -1 if str1 < str2, 1 if str1 > str2, and 0 if equal.
+ *
+ * @return  int  Returns 0 on successful execution.
+ */
 
-#include<stdio.h>
-#include<conio.h>
-#include<string.h>
-int str_compare(char* str1, char* str2);
-void main()
-{
- char str1[30], str2[30];
- int g;
- clrscr();
- printf("\n Comparing two strings \n Enter the first string:");
- scanf("%s", &str1);
- printf("\n Enter the second string:");
- scanf("%s", &str2);
- g=str_compare(str1, str2);
- if(g==-1)
- {
-  printf("\n %s is less then %s \n", str1, str2);
- }
- else if(g==1)
- {
-  printf("\n %s is less than %s \n", str2, str1);
- }
- else
- {
-  printf("\n %s and %s is equal \n", str1, str2);
- }
- getch();
+#include <conio.h> /* Console I/O library for clrscr() and getch() (DOS/Turbo C specific) */
+#include <stdio.h>  /* Standard I/O library for printf and scanf functions */
+#include <string.h> /* String library */
+
+/**
+ * @brief   Compares two strings based on ASCII value sum.
+ * @param   str1  First string to compare.
+ * @param   str2  Second string to compare.
+ * @return  int   -1 if str1 < str2, 1 if str1 > str2, 0 if equal.
+ */
+int str_compare(char *str1, char *str2);
+
+/**
+ * @brief   Main function - Entry point of the program.
+ *
+ * Prompts user for two strings and displays comparison result.
+ */
+int main() {
+  char str1[30], str2[30]; /* Input strings */
+  int g;                   /* Comparison result */
+
+  clrscr(); /* Clears the console screen (Turbo C specific) */
+
+  /* Input first string */
+  printf("\n Comparing two strings \n Enter the first string:");
+  scanf("%s", str1);
+
+  /* Input second string */
+  printf("\n Enter the second string:");
+  scanf("%s", str2);
+
+  /* Compare the strings */
+  g = str_compare(str1, str2);
+
+  /* Display result based on comparison */
+  if (g == -1) {
+    printf("\n %s is less than %s \n", str1, str2);
+  } else if (g == 1) {
+    printf("\n %s is less than %s \n", str2, str1);
+  } else {
+    printf("\n %s and %s are equal \n", str1, str2);
+  }
+
+  getch(); /* Wait for a key press before closing (Turbo C specific) */
+
+  return 0; /* Return 0 to indicate successful execution */
 }
-int str_compare(char* str1, char* str2)
-{
- int i,ascii1=0,ascii2=0;
- for(i=0;str1[i]!='\0';i++)
- ascii1=ascii1+str1[i];
- for(i=0;str2[i]!='\0';i++)
- ascii2=ascii2+str2[i];
- if(ascii1<ascii2)
- {
-  return -1;
- }
- else if(ascii1>ascii2)
- {
-  return 1;
- }
- else
- {
-  return 0;
- }
+
+/**
+ * @brief   Compares two strings by calculating ASCII value sums.
+ *
+ * Sums all ASCII values of each character in both strings
+ * and compares the totals.
+ *
+ * @param   str1  First string to compare.
+ * @param   str2  Second string to compare.
+ * @return  int   -1 if str1 < str2, 1 if str1 > str2, 0 if equal.
+ */
+int str_compare(char *str1, char *str2) {
+  int i, ascii1 = 0, ascii2 = 0;
+
+  /* Calculate ASCII sum for first string */
+  for (i = 0; str1[i] != '\0'; i++)
+    ascii1 = ascii1 + str1[i];
+
+  /* Calculate ASCII sum for second string */
+  for (i = 0; str2[i] != '\0'; i++)
+    ascii2 = ascii2 + str2[i];
+
+  /* Compare and return result */
+  if (ascii1 < ascii2) {
+    return -1;
+  } else if (ascii1 > ascii2) {
+    return 1;
+  } else {
+    return 0;
+  }
 }

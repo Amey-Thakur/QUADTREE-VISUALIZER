de6553a86d57d931409e1183fa0f968fa60782f3
Amey Thakur <ameythakur20@gmail.com>
2026-01-06 20:14:40 -0500

    QuadTree Visualizer

--- Initial Work/Phi Education/Introduction To C Programming/Milestone_5/strfindsubstring.C
+++ Initial Work/Phi Education/Introduction To C Programming/Milestone_5/strfindsubstring.C
@@ -1,62 +1,132 @@
-//write a program for int str_find_substring(char *str1, char *str2) - returns the position where str2 is in str1 and if not present returns -1. E.g. if str1 is "Hello World" and str2 is "World" then the function should return 6 and if str2 is "eat" then it should return -1
+/**
+ * @file    strfindsubstring.C
+ * @brief   Finds the position of a substring within a string.
+ *
+ * @author  Amey Thakur
+ *          GitHub: https://github.com/Amey-Thakur
+ * @author  Mega Satish
+ *          GitHub: https://github.com/msatmod
+ * @author  Hasan Rizvi
+ *          GitHub: https://github.com/rizvihasan
+ *
+ * @project QUADTREE-VISUALIZER
+ * @group   Phi-CS-73
+ * @batch   2022
+ * @repo    https://github.com/Amey-Thakur/QUADTREE-VISUALIZER
+ * @date    2021
+ * @license MIT
+ *
+ * Developed as part of the Phi Education Training (Milestone 5) and
+ * BE Major-Project @ Terna Engineering College, University of Mumbai.
+ *
+ * This program searches for a substring within a main string and returns
+ * its starting position if found, or -1 if not present.
+ * Example: "Hello World" with substring "World" returns 6.
+ *
+ * @return  int  Returns 0 on successful execution.
+ */
 
-#include<stdio.h>
-#include<conio.h>
-#include<string.h>
-int str_length(char* str);
-int str_find_substring(char* str, char* sub);
-void main()
-{
- char sub[30],str[30];
- int f;
- clrscr();
- printf("Enter the string:");
- scanf("%s", &str);
- printf("\nEnter the substring want to search:");
- scanf("%s", &sub);
- f=str_find_substring(str,sub);
- if(f==-1)
- {
-  printf("\n %s is not present in the string\n", sub);
- }
- else
- {
-  printf("\n the substring is presnt at position %d\n", f+1);
- }
- getch();
-}
-int str_length(char* str)
-{
- int len=0,i=0;
- while(str[i]!='\0')
- {
-  len++;
-  i++;
- }
- return len;
-}
-int str_find_substring(char* str, char* sub)
-{
- int i,j,len;
- len=str_length(sub);
- for(i=0,j=0;str[i]!='\0'&&sub[j]!='\0';i++)
- {
-  if(str[i]==sub[j])
-  {
-   j++;
+#include <conio.h> /* Console I/O library for clrscr() and getch() (DOS/Turbo C specific) */
+#include <stdio.h>  /* Standard I/O library for printf and scanf functions */
+#include <string.h> /* String library */
+
+/**
+ * @brief   Calculates the length of a string.
+ * @param   str  The string to measure.
+ * @return  int  The length of the string.
+ */
+int str_length(char *str);
+
+/**
+ * @brief   Finds a substring within a string.
+ * @param   str  The main string to search in.
+ * @param   sub  The substring to find.
+ * @return  int  Starting position if found, -1 otherwise.
+ */
+int str_find_substring(char *str, char *sub);
+
+/**
+ * @brief   Main function - Entry point of the program.
+ *
+ * Prompts user for a string and substring, then searches.
+ */
+int main() {
+  char sub[30], str[30]; /* str: main string; sub: substring to find */
+  int f;                 /* Result: position or -1 */
+
+  clrscr(); /* Clears the console screen (Turbo C specific) */
+
+  /* Input main string */
+  printf("Enter the string:");
+  scanf("%s", str);
+
+  /* Input substring to search */
+  printf("\nEnter the substring to search:");
+  scanf("%s", sub);
+
+  /* Search for the substring */
+  f = str_find_substring(str, sub);
+
+  /* Display result */
+  if (f == -1) {
+    printf("\n %s is not present in the string\n", sub);
+  } else {
+    printf("\n The substring is present at position %d\n", f + 1);
   }
-  else
-  {
-   j=0;
+
+  getch(); /* Wait for a key press before closing (Turbo C specific) */
+
+  return 0; /* Return 0 to indicate successful execution */
+}
+
+/**
+ * @brief   Calculates the length of a string.
+ *
+ * Counts characters until null terminator is reached.
+ *
+ * @param   str  The string to measure.
+ * @return  int  The length of the string.
+ */
+int str_length(char *str) {
+  int len = 0, i = 0;
+
+  while (str[i] != '\0') {
+    len++;
+    i++;
   }
- }
- if(j==len)
- {
-  int x=i-j;
-  return x;
- }
- else
- {
-  return -1;
- }
+
+  return len;
+}
+
+/**
+ * @brief   Finds a substring within a string using sequential matching.
+ *
+ * Iterates through the main string and tracks consecutive matches
+ * with the substring characters.
+ *
+ * @param   str  The main string to search in.
+ * @param   sub  The substring to find.
+ * @return  int  Starting position (0-indexed) if found, -1 otherwise.
+ */
+int str_find_substring(char *str, char *sub) {
+  int i, j, len;
+
+  len = str_length(sub); /* Get substring length */
+
+  /* Search for matching sequence */
+  for (i = 0, j = 0; str[i] != '\0' && sub[j] != '\0'; i++) {
+    if (str[i] == sub[j]) {
+      j++; /* Match found, move to next substring character */
+    } else {
+      j = 0; /* Mismatch, reset substring index */
+    }
+  }
+
+  /* Check if entire substring was matched */
+  if (j == len) {
+    int x = i - j; /* Calculate starting position */
+    return x;
+  } else {
+    return -1; /* Substring not found */
+  }
 }

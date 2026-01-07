2aaa6af866ccfaa59812f1c1141ce8676d21ad98
Amey Thakur <ameythakur20@gmail.com>
2026-01-06 20:26:44 -0500

    QuadTree Visualizer

--- Initial Work/Phi Education/Introduction To C Programming/Milestone_9/movenode.C
+++ Initial Work/Phi Education/Introduction To C Programming/Milestone_9/movenode.C
@@ -1,53 +1,108 @@
-#include<stdio.h>
-#include<conio.h>
-#include<stdlib.h>
-struct Node
-{
- int data;
- struct Node* next;
+/**
+ * @file    movenode.C
+ * @brief   Moves the front node from one linked list to another.
+ *
+ * @author  Amey Thakur
+ *          GitHub: https://github.com/Amey-Thakur
+ * @author  Mega Satish
+ *          GitHub: https://github.com/msatmod
+ * @author  Hasan Rizvi
+ *          GitHub: https://github.com/rizvihasan
+ *
+ * @project QUADTREE-VISUALIZER
+ * @group   Phi-CS-73
+ * @batch   2022
+ * @repo    https://github.com/Amey-Thakur/QUADTREE-VISUALIZER
+ * @date    2021
+ * @license MIT
+ *
+ * Developed as part of the Phi Education Training (Milestone 9) and
+ * BE Major-Project @ Terna Engineering College, University of Mumbai.
+ *
+ * This program demonstrates the MoveNode operation which takes the
+ * front node from the source list and moves it to the destination list.
+ *
+ * @return  int  Returns 0 on successful execution.
+ */
+
+#include <conio.h> /* Console I/O library for clrscr() and getch() (DOS/Turbo C specific) */
+#include <stdio.h>  /* Standard I/O library for printf functions */
+#include <stdlib.h> /* Standard library for malloc() */
+
+/**
+ * @brief   Node structure for linked list.
+ */
+struct Node {
+  int data;          /* Data stored in node */
+  struct Node *next; /* Pointer to next node */
 };
-void printList(struct Node* head)
-{
- struct Node* ptr=head;
- while(ptr)
- {
-  printf("%d->",ptr->data);
-  ptr=ptr->next;
- }
- printf("NULL\n");
+
+/**
+ * @brief   Prints all nodes in the linked list.
+ */
+void printList(struct Node *head) {
+  struct Node *ptr = head;
+  while (ptr) {
+    printf("%d->", ptr->data);
+    ptr = ptr->next;
+  }
+  printf("NULL\n");
 }
-void push(struct Node** head, int data)
-{
- struct Node* newNode=(struct Node*)malloc(sizeof(struct Node));
- newNode->data=data;
- newNode->next=*head;
- *head=newNode;
+
+/**
+ * @brief   Pushes a new node to the front of the list.
+ */
+void push(struct Node **head, int data) {
+  struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+  newNode->data = data;
+  newNode->next = *head;
+  *head = newNode;
 }
-void MoveNode(struct Node** destRef, struct Node** sourceRef)
-{
- struct Node* newNode=*sourceRef;
- if(*sourceRef==NULL)
- return;
- *sourceRef=(*sourceRef)->next;
- newNode->next=*destRef;
- *destRef=newNode;
+
+/**
+ * @brief   Moves the front node from source to destination list.
+ */
+void MoveNode(struct Node **destRef, struct Node **sourceRef) {
+  struct Node *newNode = *sourceRef;
+
+  if (*sourceRef == NULL)
+    return;
+
+  *sourceRef = (*sourceRef)->next; /* Advance source */
+  newNode->next = *destRef;        /* Link to destination */
+  *destRef = newNode;              /* Update destination head */
 }
-void main()
-{
- int i,n;
- struct node* a=NULL;
- struct node* b=NULL;
- int keys[]={1,2,3};
- n=sizeof(keys)/sizeof(keys[0]);
- clrscr();
- for(i=n-1;i>=0;i--)
- push(&a,keys[i]);
- for(i=0;i<n;i++)
- push(&b,2*keys[i]);
- MoveNode(&a, &b);
- printf("First List:");
- printList(a);
- printf("Second List:");
- printList(b);
- getch();
+
+/**
+ * @brief   Main function - Entry point of the program.
+ */
+int main() {
+  int i, n;
+  struct Node *a = NULL;
+  struct Node *b = NULL;
+  int keys[] = {1, 2, 3};
+
+  n = sizeof(keys) / sizeof(keys[0]);
+
+  clrscr(); /* Clears the console screen (Turbo C specific) */
+
+  /* Build list a: 1 -> 2 -> 3 */
+  for (i = n - 1; i >= 0; i--)
+    push(&a, keys[i]);
+
+  /* Build list b: 2 -> 4 -> 6 */
+  for (i = 0; i < n; i++)
+    push(&b, 2 * keys[i]);
+
+  /* Move front node from b to a */
+  MoveNode(&a, &b);
+
+  printf("First List:");
+  printList(a);
+  printf("Second List:");
+  printList(b);
+
+  getch(); /* Wait for a key press before closing (Turbo C specific) */
+
+  return 0; /* Return 0 to indicate successful execution */
 }
--- Initial Work/Phi Education/Introduction To C Programming/Milestone_9/pop.C
+++ Initial Work/Phi Education/Introduction To C Programming/Milestone_9/pop.C
@@ -1,50 +1,104 @@
-#include<stdio.h>
-#include<conio.h>
-#include<stdlib.h>
-struct Node
-{
- int data;
- struct Node* next;
+/**
+ * @file    pop.C
+ * @brief   Removes and returns the front node from a linked list.
+ *
+ * @author  Amey Thakur
+ *          GitHub: https://github.com/Amey-Thakur
+ * @author  Mega Satish
+ *          GitHub: https://github.com/msatmod
+ * @author  Hasan Rizvi
+ *          GitHub: https://github.com/rizvihasan
+ *
+ * @project QUADTREE-VISUALIZER
+ * @group   Phi-CS-73
+ * @batch   2022
+ * @repo    https://github.com/Amey-Thakur/QUADTREE-VISUALIZER
+ * @date    2021
+ * @license MIT
+ *
+ * Developed as part of the Phi Education Training (Milestone 9) and
+ * BE Major-Project @ Terna Engineering College, University of Mumbai.
+ *
+ * This program demonstrates the pop operation which removes and
+ * returns the data from the front node of a linked list.
+ *
+ * @return  int  Returns 0 on successful execution.
+ */
+
+#include <conio.h> /* Console I/O library for clrscr() and getch() (DOS/Turbo C specific) */
+#include <stdio.h>  /* Standard I/O library for printf functions */
+#include <stdlib.h> /* Standard library for malloc() and free() */
+
+/**
+ * @brief   Node structure for linked list.
+ */
+struct Node {
+  int data;          /* Data stored in node */
+  struct Node *next; /* Pointer to next node */
 };
-void printList(struct Node* head)
-{
- struct Node* ptr=head;
- while(ptr)
- {
-  printf("%d->", ptr->data);
-  ptr=ptr->next;
- }
- printf("null");
+
+/**
+ * @brief   Prints all nodes in the linked list.
+ */
+void printList(struct Node *head) {
+  struct Node *ptr = head;
+  while (ptr) {
+    printf("%d->", ptr->data);
+    ptr = ptr->next;
+  }
+  printf("null");
 }
-void push(struct Node** head, int data)
-{
- struct Node* newNode=(struct Node*)malloc(sizeof(struct Node));
- newNode->data=data;
- newNode->next=*head;
- *head=newNode;
+
+/**
+ * @brief   Pushes a new node to the front of the list.
+ */
+void push(struct Node **head, int data) {
+  struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+  newNode->data = data;
+  newNode->next = *head;
+  *head = newNode;
 }
-int pop(struct Node** headRef)
-{
- int result;
- struct Node* head=*headRef;
- if(*headRef==NULL)
- return -1;
- result=head->data;
- (*headRef)=(*headRef)->next;
- free(head);
- return result;
+
+/**
+ * @brief   Pops (removes and returns) the front node's data.
+ *
+ * @param   headRef  Pointer to head pointer.
+ * @return  int      Data from the popped node, or -1 if empty.
+ */
+int pop(struct Node **headRef) {
+  int result;
+  struct Node *head = *headRef;
+
+  if (*headRef == NULL)
+    return -1;
+
+  result = head->data;
+  (*headRef) = (*headRef)->next;
+  free(head);
+
+  return result;
 }
-void main()
-{
- int keys[]={1,2,3,4};
- int n,i;
- struct Node* head=NULL;
- clrscr();
- n=sizeof(keys)/sizeof(keys[0]);
- for(i=n-1;i>=0;i--)
- push(&head,keys[i]);
- i=pop(&head);
- printf("Popped node %d\n",i);
- printList(head);
- getch();
+
+/**
+ * @brief   Main function - Entry point of the program.
+ */
+int main() {
+  int keys[] = {1, 2, 3, 4};
+  int n, i;
+  struct Node *head = NULL;
+
+  clrscr(); /* Clears the console screen (Turbo C specific) */
+
+  n = sizeof(keys) / sizeof(keys[0]);
+  for (i = n - 1; i >= 0; i--)
+    push(&head, keys[i]);
+
+  /* Pop the front node */
+  i = pop(&head);
+  printf("Popped node %d\n", i);
+  printList(head);
+
+  getch(); /* Wait for a key press before closing (Turbo C specific) */
+
+  return 0; /* Return 0 to indicate successful execution */
 }
